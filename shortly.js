26a85e7032d5786e3e493c410ae44f2958534e90
shadowfool <aaronf101@gmail.com>
2016-05-17 11:48:44 -0700

    Implemented OAuth

--- shortly.js
+++ shortly.js
@@ -5,6 +5,7 @@ var util = require('./lib/utility');
 var partials = require('express-partials');
 var bodyParser = require('body-parser');
 var url = require('url');
+
 var knex = require('knex')({
   client: 'sqlite3',
   connection: {
@@ -13,6 +14,11 @@ var knex = require('knex')({
   useNullAsDefault: true
 });
 
+var passport = require('passport');
+var gitHubStrategy = require('passport-github2').Strategy;
+
+var GITHUB_CLIENT_ID = "5791dc9c76d58ac0ec75";
+var GITHUB_CLIENT_SECRET = "5646c244d810ea413c27c150645095de17cb8048";
 
 var db = require('./app/config');
 var Users = require('./app/collections/users');
@@ -32,16 +38,35 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
-var sessionOptions = {secret:"secret"};
+var sessionOptions = { secret: 'secret' };
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
 
 app.use(session(sessionOptions));
 
+passport.use(new gitHubStrategy({
+  clientID: GITHUB_CLIENT_ID,
+  clientSecret: GITHUB_CLIENT_SECRET,
+  callbackURL: 'http://127.0.0.1:4568/auth/github/callback'
+}, function(accessToken, refreshToken, profile, done) { 
+  process.nextTick(function() {
+    return done(null, profile);
+  });
+}));
 
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 app.get('/', 
 function(req, res) {
-  if (req.session.username) {
+  if (util.checkUser(req, res)) {
     res.render('index');
   } else {
     res.redirect('/login');
@@ -59,7 +84,7 @@ function(req, res) {
 
 app.get('/login', 
 function(req, res) {
-  if (req.session.username) {
+  if (util.checkUser(req, res)) {
     res.render('index');
   } else {
     res.render('login');
@@ -74,7 +99,7 @@ function(req, res) {
 
 app.get('/links', 
 function(req, res) {
-  if (req.session.username) {
+  if (util.checkUser(req, res)) {
     Links.reset().fetch().then(function(links) {
       res.status(200).send(links.models);
       res.end();
@@ -84,6 +109,7 @@ function(req, res) {
     res.end();
   }
 });
+
 
 app.post('/links', 
 function(req, res) {
@@ -120,6 +146,20 @@ function(req, res) {
 /************************************************************/
 // Write your authentication routes here
 /************************************************************/
+app.get('/auth/github', 
+  passport.authenticate('github', {scope: ['user:email']}),
+  function(req, res) {
+
+  }
+);
+
+app.get('/auth/github/callback',
+  passport.authenticate('github', {failureRedirect: '/login'}),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
+
 
 app.post('/login', function(req, res) {
   var username = req.body.username;
@@ -140,7 +180,6 @@ app.post('/login', function(req, res) {
 app.get('/logout', function(req, res) {
   req.session.destroy(function(err) {
     res.redirect('/');
-    res.end();
   });
 });
 
